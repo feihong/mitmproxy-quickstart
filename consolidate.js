c7941523d280493e7ca565e5f4bdf5f0c58d41c0
Feihong Hsu <feihong.hsu@gmail.com>
2019-04-20 23:17:20 -0500

    Write metadata to output file

--- consolidate.js
+++ consolidate.js
@@ -6,20 +6,26 @@ const songs = require('./assets/songs.json')
 for (const song of songs) {
   console.log(song.title);
 
+  const outputFile = `./output/${song.artist}  ${song.title}.m4a`
+
+  // Convert .ts to .m4a
   childProcess.spawnSync(
     'ffmpeg',
     [
       '-i', `./assets/${song.id}.ts`,
-      '-metadata', `title="${song.title}"`,
-      '-metadata', `author="${song.artist}"`,
-      '-metadata', `comment="${song.url}"`,
-      '-metadata', `lyrics="${song.lyrics}"`,
+      '-metadata', `title=${song.title}`,
+      '-metadata', `artist=${song.artist}`,
+      '-metadata', `comment=${song.url}`,
+      '-metadata', `lyrics=${song.lyrics}`,
+      '-metadata', 'genre=流行 Pop',
       '-vn',                // ignore video
       '-c:a', 'libfdk_aac', // use best encoder
       '-vbr', '4',          // use high quality
-      `./output/${song.artist}  ${song.title}.m4a`
+      outputFile,
     ],
     { stdio: 'inherit' },
   )
+
+  break;
 }
 
